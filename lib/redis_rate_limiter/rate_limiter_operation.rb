be885da822dde16bc26162ef98268fed616847aa
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-01-11 15:11:02 +0200

    delete old buckets to get accurate count of reqs received in current window

--- lib/redis_rate_limiter/rate_limiter_operation.rb
+++ lib/redis_rate_limiter/rate_limiter_operation.rb
@@ -23,8 +23,9 @@ module RedisRateLimiter
         bucket_name = get_bucket_name(identifier)
         req_count_in_current_window = get_bucket_count(redis_key)
         if req_count_in_current_window < @max_reqs
-          redis_value = {:bucket_name => bucket_name, :ip => ip}
-          @redis_op.increment_hash_value(redis_key, JSON.generate(redis_value))
+          # redis_value = {:bucket_name => bucket_name, :ip => ip}
+          redis_value = "#{bucket_name}:#{ip}"
+          @redis_op.increment_hash_value(redis_key, redis_value)
           return true
         else
           # if requests count in current window is equal to max requests allowed in window
@@ -49,21 +50,21 @@ module RedisRateLimiter
       # we need to delete old buckets,
       # out of time.now - window_time
       # and then get the bucket count
-      # delete_old_buckets(redis_key)
+      delete_old_buckets(redis_key)
       @redis_op.get_hash_values(redis_key).map(&:to_i).reduce(:+) || 0
     end
 
-    # def delete_old_buckets(redis_key)
-    #   buckets = @redis_op.get_hash_keys(redis_key)
-    #   # delete buckets which are out of time.now - window_time
-    #   buckets.each do |bucket|
-    #     bucket_json = JSON.parse(bucket)
-    #     bucket_name = bucket_json['bucket_name']
-    #     if bucket_name.split("_")[2].to_i <= get_time - @window
-    #       @redis_op.delete_hash_by_key(redis_key, bucket_name)
-    #     end
-    #   end
-    # end
+    def delete_old_buckets(redis_key)
+      buckets = @redis_op.get_hash_keys(redis_key)
+      # delete buckets which are out of time.now - window_time
+      # bucket key format: key_identifier_time:ip
+      buckets.each do |bucket_key|
+        bucket_time = bucket_key.split(":")[0].split("_")[2].to_i
+        if bucket_time <= get_time - @window
+          @redis_op.delete_hash_by_key(redis_key, bucket_key)
+        end
+      end
+    end
 
     def get_bucket_name(identifier)
       "#{get_redis_key(identifier)}_#{get_time}"
