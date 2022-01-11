d68c090b00b149b76723a2aec9fa07062222f6bd
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-01-11 10:13:22 +0200

    initialize request rate limiter class inside redise rate limiter module

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem "redis", "~> 4.0"
 gem 'redis-namespace'
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
-
+gem 'dotenv-rails', groups: [:development, :test]
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
---
+++ lib/redis_rate_limiter/request_rate_limiter.rb
@@ -0,0 +1,18 @@
+module RedisRateLimiter
+  class RequestRateLimiter
+    # @param [String]	  key			                  A unique identifier for Limiter
+    # @param [Integer]	window_time	              Number of seconds per windows bucket > 0 sec
+    # @param [Integer]	max_requests_count	      Max Number of requests per window > 0
+    # @param [Integer]	block_time	              Number of second to block > 0 sec, default 5 minutes (300 sec)
+    def initialize(key, window_time, max_requests_count, block_time = 5 * 60 )
+      if !window_time.is_a?(Integer) && window_time.to_i <= 0
+        raise("window time value must be integer and bigger than 0")
+      end
+
+      if !max_requests_count.is_a?(Integer) && max_requests_count.to_i <= 0
+        raise("max requests value must be integer and bigger than 0")
+      end
+      @request_rate_limiter = RateLimiterOperation.new(key, window_time, max_requests_count, block_time)
+    end
+  end
+end
