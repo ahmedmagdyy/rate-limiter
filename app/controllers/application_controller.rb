3b55c8031e201096d705a0663a4348429fc66b40
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-01-11 10:53:11 +0200

    include rate limiter module in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  include RedisRateLimiter
+  before_action :track_rate_limit
+
+  def index
+    render status: OK, plain: "OK"
+  end
+
+  def track_rate_limit
+    if @limiter.track_api_usage("test", request.ip)
+      render status: 200, plain: "Allowed Request!"
+    else
+      render status: 429, plain: "Too many requests"
+    end
+  end
+
+  def get_tracked_usage
+    @limiter.tracked_usage
+  end
+
+  def init_redis_rate_limiter
+    @limiter = RedisRateLimiter::RequestRateLimiter.new("index", ENV["window_time_in_sec"], ENV["allowed_req_per_window"], ENV["block_time_in_sec"])
+  end
 end
