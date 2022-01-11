566609abd2ce38d3313ee784b5c39e44de30ed97
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-01-11 10:33:02 +0200

    add function to get all hash keys from redis

--- lib/redis_rate_limiter/redis_operation.rb
+++ lib/redis_rate_limiter/redis_operation.rb
@@ -8,6 +8,10 @@ module RedisRateLimiter
       @redis.hget(key)
     end
 
+    def get_hash_keys(key)
+      @redis.hkeys(key)
+    end
+
     def delete_hash_by_key(key)
       @redis.hdel(key)
     end
