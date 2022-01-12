36e7cc53cccdd29585b36e96fb8b473ccfb5a6db
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-01-12 15:59:22 +0200

    change routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,12 @@
 class ApplicationController < ActionController::Base
   include RedisRateLimiter
   before_action :init_redis_rate_limiter
-  before_action :track_rate_limit, only: [:index]
-
-  def index
-    render status: OK, plain: "OK"
-  end
 
   def track_rate_limit
     if @limiter.track_api_usage("test", request.ip)
       render status: 200, plain: "Allowed Request!"
     else
-      render status: 429, plain: "Too many requests"
+      render status: 429, plain: "Too many requests!"
     end
   end
 
@@ -20,6 +15,10 @@ class ApplicationController < ActionController::Base
   end
 
   def init_redis_rate_limiter
-    @limiter = RedisRateLimiter::RequestRateLimiter.new("home", ENV["window_time_in_sec"], ENV["allowed_req_per_window"], ENV["block_time_in_sec"])
+    @limiter = RedisRateLimiter::RequestRateLimiter.new(
+      "home",
+      ENV["window_time_in_sec"],      # default is 10 sec window
+      ENV["allowed_req_per_window"],  #default is 50 requests per window
+      ENV["block_time_in_sec"])       # 5 minutes default
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#index"
+  # root "application#index"
+  get '/track', to: 'application#track_rate_limit'
   get '/tracked', to: 'application#get_tracked_usage'
 
 end
