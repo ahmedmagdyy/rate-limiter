2560ac8dcf3930c34876db5e55886aa751907d68
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-01-12 15:58:42 +0200

    init redis using .env values

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,4 @@
-$redis = Redis::Namespace.new("rate_limiter", :redis => Redis.new)
+$redis = Redis::Namespace.new(
+  "rate_limiter",
+  :redis => Redis.new(:host => ENV["redis_host"] || "localhost", :port => ENV["redis_port"] || 6379)
+)
